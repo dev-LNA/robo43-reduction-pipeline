47f5ded0a52182489b2a687cc54b946c9fe88976
F. R. Herpich <fabiorafaelh@gmail.com>
2025-09-02 11:58:46 -0300

    Add local method to estimate astrometry to avoid constant querying to astrometry.net

--- src/process_robo43_frames.py
+++ src/process_robo43_frames.py
@@ -9,6 +9,7 @@ from astropy.wcs import WCS
 from astroquery.simbad import Simbad
 from astroquery.astrometry_net import AstrometryNet
 from photutils.detection import DAOStarFinder
+import astrometry
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
 import glob
@@ -58,6 +59,8 @@ def parse_arguments():
                         help='Attempt to solve astrometry for processed frames.')
     parser.add_argument('--sigma_clip', type=float, default=3.0,
                         help='Sigma clipping value for source detection.')
+    parser.add_argument('--object', type=str, default=None,
+                        help='Name of the target object to select images to process.')
     parser.add_argument('--runtest', action='store_true',
                         help='Run in test mode with limited files.')
     parser.add_argument('--clobber', action='store_true',
@@ -81,6 +84,7 @@ class ProcessFrame:
         self.np = args.np
         self.solve_astrometry = args.solve_astrometry
         self.sigma_clip = args.sigma_clip
+        self.object_name = args.object
         self.runtest = args.runtest
         self.clobber = args.clobber
         self.verbose = args.verbose
@@ -90,7 +94,7 @@ class ProcessFrame:
             'Initialized ProcessFrame with workdir: %s', self.workdir)
 
         self.object_names_to_guess = {
-            'eta car': ['etacar', 'eta carinae', 'etaCarNebula'],
+            'eta car': ['etacar', 'eta carinae', 'etaCarNebula', 'etacarnebula'],
         }
         self.proc_status = {}
 
@@ -110,9 +114,13 @@ class ProcessFrame:
                         self.logger.debug(
                             'Will overwrite existing processed file: %s', proc_file_name)
                 else:
-                    self.logger.info(
-                        'Some processed files already exist. Use --clobber to overwrite.')
-                    sys.exit(1)
+                    if self.solve_astrometry:
+                        self.logger.info(
+                            'Processed file exists, but will attempt astrometry solving: %s', proc_file_name)
+                    else:
+                        self.logger.info(
+                            'Some processed files already exist. Use --clobber to overwrite.')
+                        sys.exit(1)
 
             # selecting only science frames
             header = fits.getheader(f)
@@ -128,6 +136,12 @@ class ProcessFrame:
 
         fits_files = sorted([f for i, f in enumerate(fits_files)
                              if i not in exclusion_indexes and '_proc' not in f])
+        if self.object_name:
+            fits_files = [f for f in fits_files if self.object_name.lower(
+            ) == fits.getheader(f).get('OBJECT', '').lower()]
+            self.logger.info(
+                'Filtered files for object "%s", %d files remain.', self.object_name, len(fits_files))
+
         self.logger.info(
             'Selected %d science FITS files for processing.', len(fits_files))
 
@@ -255,7 +269,72 @@ class ProcessFrame:
         hdul.flush()
         return hdul
 
-    def solve_astrometry(self, path_to_fits, raw_path):
+    def run_astrometry_solver1(self, sorted_sources, hdul, raw_name):
+        with astrometry.Solver(
+            astrometry.series_5200.index_files(
+                cache_directory="/home/herpich/Documents/.astrometry", scales={6})
+        ) as solver:
+            stars = [(st['xcentroid'], st['ycentroid'])
+                     for st in sorted_sources]
+            solution = solver.solve(stars=stars,
+                                    size_hint=astrometry.SizeHint(
+                                        lower_arcsec_per_pixel=0.2,
+                                        upper_arcsec_per_pixel=1.0),
+                                    position_hint=astrometry.PositionHint(
+                                        ra_deg=hdul[0].header.get('RA', None),
+                                        dec_deg=hdul[0].header.get(
+                                            'DEC', None),
+                                        radius_deg=1.0),
+                                    solution_parameters=astrometry.SolutionParameters())
+            if solution.has_match():
+                wcs_header = solution.best_match().astropy_wcs()
+                stars_used = {'ra': [obj.ra_deg for obj in solution.best_match().stars],
+                              'dec': [obj.dec_deg for obj in solution.best_match().stars]}
+                self.logger.info(
+                    'Astrometry solved using local index files.')
+                self.proc_status[raw_name]['proc_status'] = 'Astrometry solved'
+                self.proc_status[raw_name]['proc_code'] = 59
+                return wcs_header, stars_used
+
+    def run_astrometry_solver2(self, sorted_sources, hdul, raw_name):
+        ast = AstrometryNet()
+        img_width = hdul[0].header.get('NAXIS1', hdul[0].data.shape[1])
+        img_height = hdul[0].header.get('NAXIS2', hdul[0].data.shape[0])
+        if self.proc_status[raw_name]['proc_code'] == 7:
+            ast.ra = hdul[0].header['RA']
+            ast.dec = hdul[0].header['DEC']
+            ast.radius = 1.0
+
+        try:
+            wcs_header = ast.solve_from_source_list(
+                sorted_sources['xcentroid'], sorted_sources['ycentroid'],
+                img_width, img_height, solve_timeout=120)
+            self.logger.info('Astrometry solving completed.')
+            self.proc_status[raw_name]['proc_status'] = 'Astrometry solved'
+            self.proc_status[raw_name]['proc_code'] = 59
+        except Exception as e:
+            self.logger.error('Astrometry solving failed: %s', str(e))
+            self.proc_status[raw_name]['proc_status'] = 'Astrometry solving failed'
+            self.proc_status[raw_name]['proc_code'] = 27
+            return None, e
+        return wcs_header, sorted_sources
+
+    def run_daofinder(self, hdul):
+        try:
+            daofind = DAOStarFinder(
+                fwhm=3.0, threshold=self.sigma_clip * np.std(hdul[0].data))
+            sources = daofind(hdul[0].data - np.median(hdul[0].data))
+            sorted_sources = sources[np.argsort(sources['flux'])[::-1]]
+            self.logger.info(
+                'Detected %d sources in the image.', len(sorted_sources))
+            return sorted_sources
+        except Exception as e:
+            self.logger.error('Error detecting sources: %s', str(e))
+            self.proc_status[raw_name]['proc_status'] = 'Astrometry solving failed'
+            self.proc_status[raw_name]['proc_code'] = 27
+            return e
+
+    def solver_astrometry(self, path_to_fits, raw_path):
         self.logger.info(
             'Attempting astrometry solving for: %s', path_to_fits)
         if self.proc_status[os.path.basename(raw_path)]['proc_code'] not in [3, 7]:
@@ -272,39 +351,30 @@ class ProcessFrame:
             self.proc_status[raw_name]['proc_status'] = 'Astrometry solving failed'
             self.proc_status[raw_name]['proc_code'] = 27
             return
-        try:
-            daofind = DAOStarFinder(
-                fwhm=3.0, threshold=self.sigma_clip * np.std(hdul[0].data))
-            sources = daofind(hdul[0].data - np.median(hdul[0].data))
-            sorted_sources = sources[np.argsort(sources['flux'])[::-1]]
-            self.logger.info(
-                'Detected %d sources in the image.', len(sorted_sources))
-        except Exception as e:
-            self.logger.error('Error detecting sources: %s', str(e))
+
+        sorted_sources = self.run_daofinder(hdul)
+        if sorted_sources is None:
+            self.logger.error('No sources detected, cannot solve astrometry.')
             self.proc_status[raw_name]['proc_status'] = 'Astrometry solving failed'
             self.proc_status[raw_name]['proc_code'] = 27
             return
 
-        ast = AstrometryNet()
-        img_width = hdul[0].header.get('NAXIS1', hdul[0].data.shape[1])
-        img_height = hdul[0].header.get('NAXIS2', hdul[0].data.shape[0])
-        if self.proc_status[os.path.basename(raw_path)]['proc_code'] == 7:
-            ast.ra = hdul[0].header['RA']
-            ast.dec = hdul[0].header['DEC']
-            ast.radius = 1.0
-
+        solver_used = 0
+        astrometry_solved = False
         try:
-            wcs_header = ast.solve_from_source_list(
-                sorted_sources['xcentroid'], sorted_sources['ycentroid'],
-                img_width, img_height, solve_timeout=120)
+            wcs_header, stars_used = self.run_astrometry_solver1(
+                sorted_sources, hdul, raw_name)
+            astrometry_solved = True
+            solver_used = 1
             self.logger.info('Astrometry solving completed.')
-            self.proc_status[raw_name]['proc_status'] = 'Astrometry solved'
-            self.proc_status[raw_name]['proc_code'] = 59
         except Exception as e:
-            self.logger.error('Astrometry solving failed: %s', str(e))
-            self.proc_status[raw_name]['proc_status'] = 'Astrometry solving failed'
-            self.proc_status[raw_name]['proc_code'] = 27
-            return
+            self.logger.warning(
+                'Local astrometry solving failed: %s. Trying Astrometry.net service.', str(e))
+
+        if not astrometry_solved:
+            wcs_header, stars_used = self.run_astrometry_solver2(
+                sorted_sources, hdul, raw_name)
+            solver_used = 2
 
         # NOTE: Alternative method using image upload does not work.
         # Connection errors occur frequently and all attempts failed.
@@ -317,12 +387,19 @@ class ProcessFrame:
         # detect_threshold = 2)
         if wcs_header:
             try:
-                wcs = WCS(wcs_header)
-                hdul[0].header.update(wcs.to_header())
-                self.logger.info(
-                    'Astrometry solved and WCS updated in header.')
-                self.proc_status[raw_name]['proc_status'] = 'Astrometry solved and WCS updated'
-                self.proc_status[raw_name]['proc_code'] = 123
+                if solver_used == 1:
+                    hdul[0].header.update(wcs_header.to_header())
+                    self.logger.info(
+                        'Astrometry solved and WCS updated in header.')
+                    self.proc_status[raw_name]['proc_status'] = 'Astrometry solved and WCS updated'
+                    self.proc_status[raw_name]['proc_code'] = 123
+                elif solver_used == 2:
+                    wcs = WCS(wcs_header)
+                    hdul[0].header.update(wcs.to_header())
+                    self.logger.info(
+                        'Astrometry solved and WCS updated in header.')
+                    self.proc_status[raw_name]['proc_status'] = 'Astrometry solved and WCS updated'
+                    self.proc_status[raw_name]['proc_code'] = 123
             except Exception as e:
                 self.logger.error(
                     'Error updating WCS in header: %s', str(e))
@@ -337,8 +414,18 @@ class ProcessFrame:
 
         hdul.flush()
 
+        if solver_used == 1:
+            sources = wcs_header.all_world2pix(
+                stars_used['ra'], stars_used['dec'], 0)
+            sources = [{'xcentroid': src[0], 'ycentroid': src[1]}
+                       for src in zip(sources[0], sources[1])]
+        elif solver_used == 2:
+            sources = sorted_sources
+        else:
+            self.logger.error('No solver was successful.')
+            return
         self.plot_frame(hdul[0].data, path_to_fits,
-                        sources=sorted_sources, show=True)
+                        sources=sources, show=True)
 
         return
 
@@ -420,7 +507,7 @@ class ProcessFrame:
                         proc_code=3,
                         proc_file=os.path.basename(proc_path),
                     )
-                    self.solve_astrometry(proc_path, fits_file)
+                    self.solver_astrometry(proc_path, fits_file)
                 else:
                     self.logger.info(
                         'Astrometry already solved, skipping: %s', path_ast_solved)
@@ -463,16 +550,23 @@ class ProcessFrame:
                 self.logger.info(
                     'RA and DEC found in header: RA=%s, DEC=%s',
                     processed_data[0].header['RA'], processed_data[0].header['DEC'])
-                self.solve_astrometry(proc_path, fits_file)
+                self.solver_astrometry(proc_path, fits_file)
 
-    def organize_proc_status(self):
+        # save proc_status to CSV for each processed frame
+        out_proc_status_file = os.path.join(
+            self.output_dir, f'proc_status_{os.path.basename(fits_file).replace(".fits", "")}.csv')
+        self.organize_proc_status(output_path_file=out_proc_status_file)
+        self.logger.info('Saved processing status to: %s',
+                         out_proc_status_file)
+
+    def organize_proc_status(self, output_path_file='processing_status.csv'):
         """Organize processing status into a structured array and save it as a CSV to output_dir."""
         dtype = [('raw_file', 'U100'), ('Object', 'U100'), ('Filter', 'U20'),
                  ('RA', 'U20'), ('DEC', 'U20'), ('Exptime', 'f4'),
                  ('proc_status', 'U200'), ('proc_code', 'i4'), ('proc_file', 'U100')]
         proc_status_array = np.array(
             [tuple(v.values()) for v in self.proc_status.values()], dtype=dtype)
-        csv_file = os.path.join(self.output_dir, 'processing_status.csv')
+        csv_file = os.path.join(self.output_dir, output_path_file)
         df = pd.DataFrame(proc_status_array)
         df.to_csv(csv_file, index=False)
         self.logger.info('Saved processing status to: %s', csv_file)
@@ -489,7 +583,7 @@ class ProcessFrame:
                 with Pool(processes=self.np) as pool:
                     pool.map(self.process_frame, fits_files)
             else:
-                for fits_file in fits_files[:5]:
+                for fits_file in fits_files[:2]:
                     self.process_frame(fits_file)
         self.organize_proc_status()
         self.logger.info('Processing completed.')
